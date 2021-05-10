c11c89e9b4256cd4e291786747f62937b7482c8c
mohammed <mr.hammiddi@gmail.com>
2021-05-10 15:27:44 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 import 'package:salatime/mawaqitAPI/services.dart';
 import 'package:salatime/mawaqitAPI/mawaqit.dart';
@@ -35,6 +33,9 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+var myWith;
+var myHeight;
+
 class _MyHomePageState extends State<MyHomePage> {
   List<Mawaqit> _mawaqit;
   bool _loading;
@@ -60,153 +61,143 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Sala',
-                style: GoogleFonts.satisfy(),
-              ),
-              Text(
-                'Time',
-                style: GoogleFonts.satisfy(
-                  textStyle: TextStyle(color: Colors.amber),
+    myWith = MediaQuery.of(context).size.width;
+    myHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Sala',
+                  style: GoogleFonts.satisfy(fontSize: 50),
                 ),
-              ),
-            ],
-          ),
-        ),
-        body: Center(
-          child: _loading
-              ? SpinKitRing(
-                  color: Colors.amber,
-                  size: 40.0,
-                  lineWidth: 4,
-                )
-              : SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(30.0),
-                        child: Form(
-                          key: _oFormKey,
-                          child: Column(
-                            children: <Widget>[
-                              SelectFormField(
-                                controller: _controller,
-                                labelText: 'Wilaya',
-                                items: items,
-                                onChanged: (val) => setState(() {
-                                  _valueChanged = val;
-                                  Services.getMawaqit(_valueChanged)
-                                      .then((mawaqit) {
-                                    setState(() {
-                                      _mawaqit = mawaqit;
-                                      _loading = false;
-                                    });
-                                  });
-                                }),
-                                validator: (val) {
-                                  setState(() => _valueToValidate = val ?? '');
-                                  return null;
-                                },
-                                onSaved: (val) =>
-                                    setState(() => _valueSaved = val ?? ''),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(255, 20, 20, 20),
-                          salat: 'الفجر',
-                          mawaqit: _mawaqit[0].fajr),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(250, 20, 20, 20),
-                          salat: 'الشروق',
-                          mawaqit: _mawaqit[0].chorok),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(250, 20, 20, 20),
-                          salat: 'الظهر',
-                          mawaqit: _mawaqit[0].dhohr),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(255, 20, 20, 20),
-                          salat: 'العصر',
-                          mawaqit: _mawaqit[0].asr),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(250, 20, 20, 20),
-                          salat: 'المغرب',
-                          mawaqit: _mawaqit[0].maghrib),
-                      WaqtCard(
-                          pdng: EdgeInsets.fromLTRB(245, 20, 20, 20),
-                          salat: 'العشاء',
-                          mawaqit: _mawaqit[0].icha),
-                    ],
+                Text(
+                  'Time',
+                  style: GoogleFonts.satisfy(
+                    textStyle: TextStyle(color: Colors.amber, fontSize: 50),
                   ),
                 ),
-        ));
+              ],
+            ),
+          ),
+          body: Center(
+            child: _loading
+                ? SpinKitRing(
+                    color: Colors.amber,
+                    size: 40.0,
+                    lineWidth: 4,
+                  )
+                : SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: Form(
+                            key: _oFormKey,
+                            child: Column(
+                              children: <Widget>[
+                                SelectFormField(
+                                  controller: _controller,
+                                  labelText: 'Wilaya',
+                                  items: items,
+                                  onChanged: (val) => setState(() {
+                                    _valueChanged = val;
+                                    Services.getMawaqit(_valueChanged)
+                                        .then((mawaqit) {
+                                      setState(() {
+                                        _mawaqit = mawaqit;
+                                        _loading = false;
+                                      });
+                                    });
+                                  }),
+                                  validator: (val) {
+                                    setState(
+                                        () => _valueToValidate = val ?? '');
+                                    return null;
+                                  },
+                                  onSaved: (val) =>
+                                      setState(() => _valueSaved = val ?? ''),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        WaqtCard(salat: 'الفجر', mawaqit: _mawaqit[0].fajr),
+                        WaqtCard(
+                            salat: 'الشروق', mawaqit: _mawaqit[0].chorok),
+                        WaqtCard(salat: 'الظهر', mawaqit: _mawaqit[0].dhohr),
+                        WaqtCard(salat: 'العصر', mawaqit: _mawaqit[0].asr),
+                        WaqtCard(
+                            salat: 'المغرب', mawaqit: _mawaqit[0].maghrib),
+                        WaqtCard(salat: 'العشاء', mawaqit: _mawaqit[0].icha),
+                      ],
+                    ),
+                  ),
+          )),
+    );
   }
 }
 
 class WaqtCard extends StatelessWidget {
   const WaqtCard({
     Key key,
-    @required this.pdng,
     @required this.salat,
     @required String mawaqit,
   })  : _mawaqit = mawaqit,
         super(key: key);
   final String salat;
   final String _mawaqit;
-  final EdgeInsetsGeometry pdng;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Container(
-        height: 80,
-        width: 350,
-        child: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(25, 25, 0, 0),
-              child: Text(
-                _mawaqit,
-                style: GoogleFonts.anton(
-                  textStyle: TextStyle(
-                    fontSize: 30,
+        height: (myHeight * 0.1),
+        width: (myWith * 0.8),
+        child: Center(
+          child: Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(myWith * 0.04, 16, 0, 0),
+                child: Text(
+                  _mawaqit,
+                  style: GoogleFonts.anton(
+                    textStyle: TextStyle(
+                      fontSize: 25,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(170, 10, 0, 0),
-              child: Text(
-                '|',
-                style: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 50,
-                ),
-              ),
-            ),
-            Padding(
-              padding: pdng,
-              child: Text(
-                salat,
-                style: GoogleFonts.mada(
-                  textStyle: TextStyle(
-                    fontSize: 30,
+              Padding(
+                padding: EdgeInsets.fromLTRB(myWith * 0.36, 10, 0, 0),
+                child: Text(
+                  '|',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 40,
                   ),
                 ),
               ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.fromLTRB(myWith * 0.6, 20, 20, 20),
+                child: Text(
+                  salat,
+                  style: GoogleFonts.mada(
+                    textStyle:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

cdc3156e04e07ec5cc971531c63a9b6a157de288
Mohammed <mr.hammiddi@gmail.com>
2021-05-30 22:12:13 +0100

    ..

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hamidi/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hamidi/Documents/vs-github/salatime"
+export "FLUTTER_ROOT=/home/hamidi/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/hamidi/Documents/GitHub/salatime"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- lib/mawaqitAPI/mawaqit.dart
+++ lib/mawaqitAPI/mawaqit.dart
@@ -12,14 +12,14 @@ String mawaqitToJson(List<Mawaqit> data) =>
 
 class Mawaqit {
   Mawaqit({
-    this.date,
-    this.wilaya,
-    this.fajr,
-    this.chorok,
-    this.dhohr,
-    this.asr,
-    this.maghrib,
-    this.icha,
+    required this.date,
+    required this.wilaya,
+    required this.fajr,
+    required this.chorok,
+    required this.dhohr,
+    required this.asr,
+    required this.maghrib,
+    required this.icha,
   });
 
   DateTime date;
@@ -57,9 +57,9 @@ class Mawaqit {
 
 class Wilaya {
   Wilaya({
-    this.code,
-    this.arabicName,
-    this.englishName,
+    required this.code,
+    required this.arabicName,
+    required this.englishName,
   });
 
   String code;
